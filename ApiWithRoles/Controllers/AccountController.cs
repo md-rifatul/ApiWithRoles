37a627e4345aca29fdbc420d7d2f13b361220c64
Rifatul <rifatul.ism@gmail.com>
2025-08-29 22:46:11 +0600

    Implement user registration endpoint with Identity user creation in Register action

---
+++ ApiWithRoles/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿
+using ApiWithRoles.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiWithRoles.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityUser> _roleManager;
+        private readonly IConfiguration _configuration;
+
+        public AccountController(UserManager<IdentityUser> userManager, RoleManager<IdentityUser> roleManager, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _configuration = configuration;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] Register model)
+        {
+            var user = new IdentityUser
+            {
+                UserName = model.Username,
+                Email = model.Email,
+                PasswordHash = model.Password
+            };
+            var result = await _userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                return Ok(new { message = "User registered Successfully" });
+            }
+            return BadRequest(result.Errors);
+        }
+    }
+}
